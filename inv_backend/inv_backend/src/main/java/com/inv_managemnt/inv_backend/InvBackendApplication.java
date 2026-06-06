3c0e6f69f26c25561a45fa2211afcd6cdfe39722
Soumyajit Baul <jit.baul13@gmail.com>
2026-06-06 15:40:49 +0000

    spring bot backend initalization

---
+++ inv_backend/inv_backend/src/main/java/com/inv_managemnt/inv_backend/InvBackendApplication.java
@@ -0,0 +1,13 @@
+package com.inv_managemnt.inv_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvBackendApplication.class, args);
+	}
+
+}
---
+++ inv_backend/inv_backend/src/test/java/com/inv_managemnt/inv_backend/InvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inv_managemnt.inv_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
